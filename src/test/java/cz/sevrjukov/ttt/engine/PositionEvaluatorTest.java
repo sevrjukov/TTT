974d2012d7f09cc579d1a143af0ba6fea56a775e
alexandr.sevrjukov <alexandr.sevrjukov@rohlik.cz>
2021-03-14 22:28:22 +0100

    Cleanup, unit tests

--- src/test/java/cz/sevrjukov/ttt/engine/PositionEvaluatorTest.java
+++ src/test/java/cz/sevrjukov/ttt/engine/PositionEvaluatorTest.java
@@ -4,29 +4,8 @@ import cz.sevrjukov.ttt.board.Board;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertTrue;
-import static junit.framework.TestCase.fail;
 
 public class PositionEvaluatorTest {
-
-
-	@Test
-	public void testEvaluation() {
-
-		var board = new Board();
-		board.setDebug(true);
-
-		board.parseBoard("----------------"
-				+ "-------------------"
-				+ "---ox---------");
-
-//		board.printBoard();
-		var moveGenerator = new MoveGenerator();
-		var positionEvaluator = new PositionEvaluator(moveGenerator);
-		var evaluation = positionEvaluator.alphabeta(board, 7, Integer.MIN_VALUE, Integer.MAX_VALUE, false);
-
-		System.out.println("hotovo");
-
-	}
 
 
 	@Test
@@ -36,17 +15,16 @@ public class PositionEvaluatorTest {
 
 		board.parseBoard("----------------"
 				+ "--------oooo-----------"
-				+ "---x---------");
+				+ "------------");
 		board.printBoard();
 
 //		board.printBoard();
 		var moveGenerator = new MoveGenerator();
 		var positionEvaluator = new PositionEvaluator(moveGenerator);
 		var evaluation = positionEvaluator.evaluatePosition(board);
-		assertTrue(evaluation < 10000);
+		assertTrue(evaluation < -10000);
 		System.out.println(evaluation);
 	}
-
 
 
 	@Test
@@ -54,10 +32,10 @@ public class PositionEvaluatorTest {
 		var board = new Board();
 		board.parseBoard(
 				"-----------"
-						+	"-o---------"
-						+	"-o---------"
-						+	"-o---------"
-						+	"-o---------"
+						+ "-o---------"
+						+ "-o---------"
+						+ "-o---------"
+						+ "-o---------"
 		);
 		board.printBoard();
 
@@ -65,13 +43,48 @@ public class PositionEvaluatorTest {
 		var positionEvaluator = new PositionEvaluator(moveGenerator);
 		var evaluation = positionEvaluator.evaluatePosition(board);
 		System.out.println(evaluation);
-		assertTrue(evaluation < 10000);
+		assertTrue(evaluation < -10000);
+	}
 
+
+	@Test
+	public void testDiagonalFour1() {
+		var board = new Board();
+		board.parseBoard(
+				"-----------"
+						+ "-o---------"
+						+ "--o--------"
+						+ "---o-------"
+						+ "----o------"
+		);
+		board.printBoard();
+
+		var moveGenerator = new MoveGenerator();
+		var positionEvaluator = new PositionEvaluator(moveGenerator);
+		var evaluation = positionEvaluator.evaluatePosition(board);
+		System.out.println(evaluation);
+		assertTrue(evaluation < -10000);
 	}
 
 	@Test
-	public void testDiagonals() {
-		fail("impleement me");
+	public void testDiagonalFour2() {
+		var board = new Board();
+		board.parseBoard(
+				"-----------"
+						+ "----o------"
+						+ "---o-------"
+						+ "--o--------"
+						+ "-o---------"
+		);
+		board.printBoard();
+
+		var moveGenerator = new MoveGenerator();
+		var positionEvaluator = new PositionEvaluator(moveGenerator);
+		var evaluation = positionEvaluator.evaluatePosition(board);
+		System.out.println(evaluation);
+		assertTrue(evaluation < -10000);
 	}
+
+
 
 }
