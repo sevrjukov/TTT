b58c148cded8fcc4666a0d5f9dcb090c8348f5d5
Alexandr Sevrjukov <alexandr.sevrjukov@bsc-ideas.com>
2021-03-20 13:20:11 +0100

    Double buffer rendering

--- src/main/java/cz/sevrjukov/ttt/gui/GameBoardCanvas.java
+++ src/main/java/cz/sevrjukov/ttt/gui/GameBoardCanvas.java
@@ -7,149 +7,165 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.image.BufferStrategy;
 
 import static cz.sevrjukov.ttt.board.Board.COMPUTER;
 
 public class GameBoardCanvas extends Canvas {
 
-	public static final int NUM_CELLS = 19;
-	public static final int FIELD_SIZE = 30;
+    public static final int NUM_CELLS = 19;
+    public static final int FIELD_SIZE = 30;
 
-	private static final int DIMENSION = NUM_CELLS * FIELD_SIZE;
+    private static final int DIMENSION = NUM_CELLS * FIELD_SIZE;
 
-	private static final int CIRCLE_SIZE = FIELD_SIZE - 12;
-	private static final int CIRCLE_OFFSET = (FIELD_SIZE - CIRCLE_SIZE) / 2;
-	private static final int LINES_OFFSET = 6;
-	private static final int WINNING_LINE_OFFSET = (FIELD_SIZE / 2) + 1;
+    private static final int CIRCLE_SIZE = FIELD_SIZE - 12;
+    private static final int CIRCLE_OFFSET = (FIELD_SIZE - CIRCLE_SIZE) / 2;
+    private static final int LINES_OFFSET = 6;
+    private static final int WINNING_LINE_OFFSET = (FIELD_SIZE / 2) + 1;
 
 
-	private static final Color BACKGROUND_COLOR = new Color(199, 217, 211);
-	private static final Color LINES_COLOR = new Color(237, 247, 244);
-	private static final Color BLUE_CIRCLE_COLOR = new Color(95, 141, 218);
-	private static final Color ORANGE_CIRCLE_COLOR = new Color(161, 86, 28);
-	private static final Color LAST_MOVE_COLOR = new Color(218, 240, 233);
+    private static final Color BACKGROUND_COLOR = new Color(199, 217, 211);
+    private static final Color LINES_COLOR = new Color(237, 247, 244);
+    private static final Color BLUE_CIRCLE_COLOR = new Color(95, 141, 218);
+    private static final Color ORANGE_CIRCLE_COLOR = new Color(161, 86, 28);
+    private static final Color LAST_MOVE_COLOR = new Color(218, 240, 233);
 
-	private BoardModel boardModel;
+    private BoardModel boardModel;
 
-	public GameBoardCanvas(BoardModel boardModel) {
-		this.boardModel = boardModel;
+    public GameBoardCanvas(BoardModel boardModel) {
+        this.boardModel = boardModel;
+    }
+
+    public void init() {
+		createBufferStrategy(2);
 	}
 
-	@Override
-	public void paint(Graphics g) {
+    @Override
+    public void paint(Graphics g) {
 
-		g.setColor(BACKGROUND_COLOR);
 
-		g.fillRoundRect(0, 0, DIMENSION, DIMENSION, 20, 20);
-		paintLines();
 
-		var movesList = boardModel.getMovesList();
+		var bufferStrategy = getBufferStrategy();
+		var graphics = bufferStrategy.getDrawGraphics();
+		try {
+			final Graphics2D g2 = setRenderingHints((Graphics2D) graphics);
 
-		for (int moveNum = 0; moveNum < movesList.size(); moveNum++) {
-			var move = movesList.get(moveNum);
-			if (moveNum == movesList.size() - 1 && move.side == COMPUTER) {
-				drawLastMove(move.squareNum, move.side);
-			} else {
-				drawMove(move.squareNum, move.side);
-			}
+			paintBackground(g2);
+			paintMoves(g2);
+			paintWinningSequence(g2);
+		} finally {
+			graphics.dispose();
 		}
+		bufferStrategy.show();
 
-		if (boardModel.isWinningPosition()) {
-			int[] line = boardModel.getWinningSequence();
-			paintWinningLine(line[0], line[1]);
-		}
+    }
 
-	}
+    private void paintBackground(Graphics2D g) {
+        g.setColor(BACKGROUND_COLOR);
+        g.fillRoundRect(0, 0, DIMENSION, DIMENSION, 20, 20);
+        paintLines(g);
+    }
 
-	private void paintWinningLine(int startSqNum, int endSqNum) {
-		var g = getGraphics();
-		setRenderingHints(g);
-		g.setColor(ORANGE_CIRCLE_COLOR);
+    private void paintMoves(Graphics2D g) {
 
-		int startSqX = startSqNum % NUM_CELLS;
-		int startSqY = startSqNum / NUM_CELLS;
-		int x1 = startSqX * FIELD_SIZE + WINNING_LINE_OFFSET;
-		int y1 = startSqY * FIELD_SIZE + WINNING_LINE_OFFSET;
+        var movesList = boardModel.getMovesList();
 
-		int endSqX = endSqNum % NUM_CELLS;
-		int endSqY = endSqNum / NUM_CELLS;
+        for (int moveNum = 0; moveNum < movesList.size(); moveNum++) {
+            var move = movesList.get(moveNum);
+            if (moveNum == movesList.size() - 1 && move.side == COMPUTER) {
+                paintLastMove(g, move.squareNum, move.side);
+            } else {
+                paintMove(g, move.squareNum, move.side);
+            }
+        }
+    }
 
-		int x2 = endSqX * FIELD_SIZE + WINNING_LINE_OFFSET;
-		int y2 = endSqY * FIELD_SIZE + WINNING_LINE_OFFSET;
+    private void paintWinningSequence(Graphics2D g) {
+        if (boardModel.isWinningPosition()) {
+            int[] line = boardModel.getWinningSequence();
+            paintWinningLine(g, line[0], line[1]);
+        }
+    }
 
-		Graphics2D g2 = (Graphics2D) g;
-		g2.setStroke(new BasicStroke(3));
-		g2.drawLine(x1, y1, x2, y2);
+    private void paintWinningLine(Graphics2D g, int startSqNum, int endSqNum) {
 
-	}
+        g.setColor(ORANGE_CIRCLE_COLOR);
 
+        int startSqX = startSqNum % NUM_CELLS;
+        int startSqY = startSqNum / NUM_CELLS;
+        int x1 = startSqX * FIELD_SIZE + WINNING_LINE_OFFSET;
+        int y1 = startSqY * FIELD_SIZE + WINNING_LINE_OFFSET;
 
-	@Override
-	public Dimension getPreferredSize() {
-		return new Dimension(DIMENSION, DIMENSION);
-	}
+        int endSqX = endSqNum % NUM_CELLS;
+        int endSqY = endSqNum / NUM_CELLS;
 
+        int x2 = endSqX * FIELD_SIZE + WINNING_LINE_OFFSET;
+        int y2 = endSqY * FIELD_SIZE + WINNING_LINE_OFFSET;
 
-	public void drawMove(int squareNum, int side) {
-		// square coordinates
-		int sqX = squareNum % NUM_CELLS;
-		int sqY = squareNum / NUM_CELLS;
-
-		int coordX = sqX * FIELD_SIZE + CIRCLE_OFFSET + 1;
-		int coordY = sqY * FIELD_SIZE + CIRCLE_OFFSET + 1;
-
-		Color circleColor = (side == COMPUTER) ? ORANGE_CIRCLE_COLOR : BLUE_CIRCLE_COLOR;
-		drawCircle(coordX, coordY, circleColor);
-	}
-
-	// highlighted one
-	public void drawLastMove(int squareNum, int size) {
-
-		// draw a small highlighted square
-		int sqX = squareNum % NUM_CELLS;
-		int sqY = squareNum / NUM_CELLS;
-
-		int coordX = sqX * FIELD_SIZE;
-		int coordY = sqY * FIELD_SIZE;
-
-		Graphics g = getGraphics();
-		g.setColor(LAST_MOVE_COLOR);
-
-		g.fillRect(coordX, coordY, FIELD_SIZE, FIELD_SIZE);
-
-		drawMove(squareNum, size);
-	}
-
-	private void drawCircle(int x, int y, Color color) {
-		var g = setRenderingHints(getGraphics());
-		g.setColor(color);
-		g.fillOval(x, y, CIRCLE_SIZE, CIRCLE_SIZE);
-	}
+        g.setStroke(new BasicStroke(3));
+        g.drawLine(x1, y1, x2, y2);
+    }
 
 
-	private void paintLines() {
-		var g = setRenderingHints(getGraphics());
-		g.setColor(LINES_COLOR);
-		// verticals
-		var offset = 0;
-		for (int i = 0; i < NUM_CELLS - 1; i++) {
-			offset += FIELD_SIZE;
-			g.drawLine(offset, LINES_OFFSET, offset, DIMENSION - LINES_OFFSET);
-			g.drawLine(LINES_OFFSET, offset, DIMENSION - LINES_OFFSET, offset);
-		}
-	}
-
-	private Graphics2D setRenderingHints(Graphics g) {
-		Graphics2D g2 = (Graphics2D) g;
-		RenderingHints rh = new RenderingHints(
-				RenderingHints.KEY_ANTIALIASING,
-				RenderingHints.VALUE_ANTIALIAS_ON);
-		rh.add(new RenderingHints(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY));
-		rh.add(new RenderingHints(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE));
-		rh.add(new RenderingHints(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC));
-		g2.setRenderingHints(rh);
-		return g2;
-	}
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(DIMENSION, DIMENSION);
+    }
 
 
+    public void paintMove(Graphics2D g, int squareNum, int side) {
+        // square coordinates
+        int sqX = squareNum % NUM_CELLS;
+        int sqY = squareNum / NUM_CELLS;
+
+        int coordX = sqX * FIELD_SIZE + CIRCLE_OFFSET + 1;
+        int coordY = sqY * FIELD_SIZE + CIRCLE_OFFSET + 1;
+
+        Color circleColor = (side == COMPUTER) ? ORANGE_CIRCLE_COLOR : BLUE_CIRCLE_COLOR;
+        paintCircle(g, coordX, coordY, circleColor);
+    }
+
+    // highlighted one
+    public void paintLastMove(Graphics2D g, int squareNum, int size) {
+
+        // draw a small highlighted square
+        int sqX = squareNum % NUM_CELLS;
+        int sqY = squareNum / NUM_CELLS;
+
+        int coordX = sqX * FIELD_SIZE;
+        int coordY = sqY * FIELD_SIZE;
+
+        g.setColor(LAST_MOVE_COLOR);
+        g.fillRect(coordX, coordY, FIELD_SIZE, FIELD_SIZE);
+
+        paintMove(g, squareNum, size);
+    }
+
+    private void paintCircle(Graphics2D g, int x, int y, Color color) {
+        g.setColor(color);
+        g.fillOval(x, y, CIRCLE_SIZE, CIRCLE_SIZE);
+    }
+
+
+    private void paintLines(Graphics g) {
+        g.setColor(LINES_COLOR);
+        // verticals
+        var offset = 0;
+        for (int i = 0; i < NUM_CELLS - 1; i++) {
+            offset += FIELD_SIZE;
+            g.drawLine(offset, LINES_OFFSET, offset, DIMENSION - LINES_OFFSET);
+            g.drawLine(LINES_OFFSET, offset, DIMENSION - LINES_OFFSET, offset);
+        }
+    }
+
+    private Graphics2D setRenderingHints(Graphics2D g2) {
+        RenderingHints rh = new RenderingHints(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        rh.add(new RenderingHints(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY));
+        rh.add(new RenderingHints(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE));
+        rh.add(new RenderingHints(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC));
+        g2.setRenderingHints(rh);
+        return g2;
+    }
 }
--- src/main/java/cz/sevrjukov/ttt/gui/MainGameWindow.java
+++ src/main/java/cz/sevrjukov/ttt/gui/MainGameWindow.java
@@ -5,6 +5,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -15,120 +16,117 @@ import java.io.IOException;
 
 public class MainGameWindow extends JFrame {
 
-	protected JButton btnNewGame;
-	protected JButton btnMakeMove;
-	protected JButton btnSaveGames;
-	protected JTextPane infoTextPane;
-	protected JTextPane statsTextPane;
-	private GameBoardCanvas boardCanvas;
-	private GameController controller;
+    protected JButton btnNewGame;
+    protected JButton btnMakeMove;
+    protected JButton btnSaveGames;
+    protected JTextPane infoTextPane;
+    protected JTextPane statsTextPane;
+    private GameBoardCanvas boardCanvas;
+    private GameController controller;
 
-	public static void main(String[] args) {
-		//Schedule a job for the event-dispatching thread:
-		//creating and showing this application's GUI.
-		MainGameWindow o = new MainGameWindow();
-		o.init();
-	}
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        MainGameWindow o = new MainGameWindow();
+        o.init();
+    }
 
-	public void init() {
-		controller = new GameController(this);
-		javax.swing.SwingUtilities.invokeLater(() -> {
-			try {
-				createAndShowGUI();
-				controller.init();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		});
+    public void init() {
+        controller = new GameController(this);
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            createAndShowGUI();
+            controller.init();
+        });
 
-	}
+    }
 
-	private void createAndShowGUI() throws IOException {
-		this.setTitle("Gomoku");
-		this.setBounds(100, 100, 1000, 640);
-		this.setResizable(false);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    private void createAndShowGUI() {
+        this.setTitle("Gomoku");
+        this.setBounds(100, 100, 1000, 640);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		BorderLayout borderLayout = new BorderLayout();
-		this.getContentPane().setLayout(borderLayout);
+        BorderLayout borderLayout = new BorderLayout();
+        this.getContentPane().setLayout(borderLayout);
 
-		var leftSidePannel = new JPanel();
-		leftSidePannel.setPreferredSize(new Dimension(310, 400));
+        var leftSidePannel = new JPanel();
+        leftSidePannel.setPreferredSize(new Dimension(310, 400));
 
-		this.getContentPane().add(leftSidePannel, BorderLayout.WEST);
+        this.getContentPane().add(leftSidePannel, BorderLayout.WEST);
 
-		btnNewGame = new JButton("New game");
-		btnNewGame.addActionListener(controller);
-		leftSidePannel.add(btnNewGame);
+        btnNewGame = new JButton("New game");
+        btnNewGame.addActionListener(controller);
+        leftSidePannel.add(btnNewGame);
 
-		btnMakeMove = new JButton("Make first move");
-		btnMakeMove.addActionListener(controller);
-		leftSidePannel.add(btnMakeMove);
+        btnMakeMove = new JButton("Make first move");
+        btnMakeMove.addActionListener(controller);
+        leftSidePannel.add(btnMakeMove);
 
-		btnSaveGames = new JButton("Save games");
-		btnSaveGames.addActionListener(controller);
-		leftSidePannel.add(btnSaveGames);
+        btnSaveGames = new JButton("Save games");
+        btnSaveGames.addActionListener(controller);
+        leftSidePannel.add(btnSaveGames);
 
-		infoTextPane = new JTextPane();
-		infoTextPane.setPreferredSize(new Dimension(280, 200));
+        infoTextPane = new JTextPane();
+        infoTextPane.setPreferredSize(new Dimension(280, 200));
 
-		infoTextPane.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
-		infoTextPane.setEditable(false);
-		leftSidePannel.add(infoTextPane);
+        infoTextPane.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+        infoTextPane.setEditable(false);
+        leftSidePannel.add(infoTextPane);
 
-		statsTextPane = new JTextPane();
-		statsTextPane.setPreferredSize(new Dimension(280, 100));
-		statsTextPane.setText("Engine statistics");
-		statsTextPane.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
-		Font smallFont = new Font("Monospaced", Font.PLAIN, 10);
-		statsTextPane.setFont(smallFont);
-		statsTextPane.setForeground(Color.DARK_GRAY);
-		statsTextPane.setEditable(false);
-		leftSidePannel.add(statsTextPane);
+        statsTextPane = new JTextPane();
+        statsTextPane.setPreferredSize(new Dimension(280, 100));
+        statsTextPane.setText("Engine statistics");
+        statsTextPane.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+        Font smallFont = new Font("Monospaced", Font.PLAIN, 10);
+        statsTextPane.setFont(smallFont);
+        statsTextPane.setForeground(Color.DARK_GRAY);
+        statsTextPane.setEditable(false);
+        leftSidePannel.add(statsTextPane);
 
-		var boardPanel = new JPanel();
-		this.getContentPane().add(boardPanel, BorderLayout.CENTER);
+        var boardPanel = new JPanel();
+        this.getContentPane().add(boardPanel, BorderLayout.CENTER);
 
-		initGameBoard(boardPanel);
+        setVisible(true);
 
-		setVisible(true);
-	}
+        initGameBoard(boardPanel);
+    }
 
-	private void initGameBoard(JPanel boardPanel) {
-		boardCanvas = new GameBoardCanvas(controller.getBoardModel());
-		boardCanvas.addMouseListener(
-				new MouseAdapter() {
-					@Override
-					public void mouseClicked(MouseEvent e) {
-						controller.boardClicked(e);
-					}
-				});
-		boardPanel.add(boardCanvas);
-	}
+    private void initGameBoard(JPanel boardPanel) {
+        boardCanvas = new GameBoardCanvas(controller.getBoardModel());
+        boardCanvas.addMouseListener(
+                new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        controller.boardClicked(e);
+                    }
+                });
+        boardPanel.add(boardCanvas);
+        boardCanvas.init();
+    }
 
 
-	public void refreshBoard() {
-		boardCanvas.repaint();
-	}
+    public void refreshBoard() {
+        boardCanvas.repaint();
+    }
 
 
-	public void appendTextMessage(String text) {
-		var currentText = infoTextPane.getText();
-		var lines = currentText.split(System.lineSeparator());
-		if (lines.length < 11) {
-			if (currentText.isBlank()) {
-				infoTextPane.setText(text);
-			} else {
-				infoTextPane.setText(currentText + System.lineSeparator() + text);
-			}
-		} else {
-			var builder = new StringBuilder();
-			for (int i = 1; i < lines.length; i++) {
-				builder.append(lines[i].trim());
-				builder.append(System.lineSeparator());
-			}
-			builder.append(text);
-			infoTextPane.setText(builder.toString());
-		}
-	}
+    public void appendTextMessage(String text) {
+        var currentText = infoTextPane.getText();
+        var lines = currentText.split(System.lineSeparator());
+        if (lines.length < 11) {
+            if (currentText.isBlank()) {
+                infoTextPane.setText(text);
+            } else {
+                infoTextPane.setText(currentText + System.lineSeparator() + text);
+            }
+        } else {
+            var builder = new StringBuilder();
+            for (int i = 1; i < lines.length; i++) {
+                builder.append(lines[i].trim());
+                builder.append(System.lineSeparator());
+            }
+            builder.append(text);
+            infoTextPane.setText(builder.toString());
+        }
+    }
 }
